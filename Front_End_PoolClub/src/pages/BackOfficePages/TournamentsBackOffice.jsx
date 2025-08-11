13ad1b73e67e88bc3ac647ee1b3bd859fc8dae26
amine1949 <amine.barik.2001@gmail.com>
2025-08-11 19:30:21 +0100

    Add UI table for Tournament Page

--- Front_End_PoolClub/src/pages/BackOfficePages/TournamentsBackOffice.jsx
+++ Front_End_PoolClub/src/pages/BackOfficePages/TournamentsBackOffice.jsx
@@ -1,4 +1,5 @@
 import FormTournament from "../../components/BackOfficeComponents/TournamentComponents/FormTournament";
+import TournamentTable from "../../components/BackOfficeComponents/TournamentComponents/TournamentTable";
 
 const Tournaments = () => {
     return (
@@ -6,6 +7,7 @@ const Tournaments = () => {
            <div className="p-4 pt-0 mt-5">
                 <h1 className="text-3xl font-semibold text-[#538083]">Tournament Managment</h1>
                 <FormTournament />
+                <TournamentTable/>
             </div>
         </>
     );
